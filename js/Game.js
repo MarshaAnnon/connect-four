7e7ce583faff2d55cf387d88077ff888795f63c4
marsha <marshaannon@gmail.com>
2020-09-27 19:28:33 -0400

    add constructor & refactored constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,19 @@
 class Game {
+    constructor() {
+        this.board = board;
+        this.players = this.createPlayers(2);
+        this.ready = false;
+    }
 
+    /**
+     * Creates 2 player objects
+     * @return  {array}  An array of 2 player objects
+     */
+    createPlayers() {
+        const players = [
+            new Player('Player A', 1, '#e15258', true),
+            new Player('Player B', 2, '#e59a13')
+        ]       
+        return players;   
+    }
 }
