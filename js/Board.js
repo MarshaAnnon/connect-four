a1fe9a638d6adadd2fa095b30aff13cbeae32887
marsha <marshaannon@gmail.com>
2020-09-27 19:09:13 -0400

    create createSpaces method a& refactored spaces value in the constructor to this.createSpaces();

--- js/Board.js
+++ js/Board.js
@@ -1,7 +1,25 @@
 class Board {
-    constructor() {
+    constructor(row, column, spaces) {
         this.row = 6;
         this.column = 7;
-        this.spaces = [];
+        this.spaces = this.createSpaces();
+    }
+
+    /** 
+        * Generate 2D array of spaces
+        * @return  {Array}  An array os space objects
+    */
+    createSpaces() {
+        const spaces = [];
+
+        for(let x = 0; x < this.columns; x++) {
+            const column = []
+            for(let y = 0; y < this.rows; y++) {
+                const space = new Space(x, y);
+                column.push(space);
+            }
+            spaces.push(column);
+        }
+        return spaces;
     }
 }
