55e34e8bdae80e6dc990c86f7fda14beb8828b90
marsha <marshaannon@gmail.com>
2020-09-27 18:28:31 -0400

    add createTokens method and change value of tokens in constructor

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,21 @@ class Player {
         this.name = name;
         this.color = color;
         this.active = active;
-        this.tokens = [];
+        this.tokens = this.createTokens(21);
+                    // ^ this indicates that the method I am calling is available on the object that's being initialized.
+    }
+
+    /**
+     * Creates token objects for player
+     * @param  {integer} num - Number of tokens to be created
+     */ 
+    createTokens(num) {
+        const tokens = [];
+
+        for(let i = 0; i < num; i++) {
+            let token = new Token(i, this);
+            tokens.push(token)
+        }
+        return tokens;
     }
 }
